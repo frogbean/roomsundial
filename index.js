263b5a76fe822041f27ad148dee48adf6b93b39b
frogbean <49106938+frogbean@users.noreply.github.com>
2023-10-28 12:32:50 +0100

    debugging

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const { hexNow, hexUse, pClock } = require("SunPosRGB");
 const { rgblights } = require("lights");
-
 hexUse(require("./colors.json"));
 
 //globalThis.debug = true;
 //globalThis.plcktest = 0.01
 
 setInterval(()=>{
-    console.log(hexNow());
+    //console.log(pClock(), hexNow());
     rgblights(hexNow());
 }, 100);
 
